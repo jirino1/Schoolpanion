3776a1568725bf4d1be0a2ffde2568543c5ff03f
jirino1 <56354233+jirino1@users.noreply.github.com>
2020-02-26 15:22:33 +0100

    In meinem Chat wird nur deutsch gesprochen

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from 'redux-thunk'
+import thunk from "redux-thunk";
 
-import App from './App';
-import reducers from './reducers'
+import App from "./App";
+import reducers from "./reducers";
 
-const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {}
-const composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,
+const persistedState = localStorage.getItem("reduxState")
+  ? JSON.parse(localStorage.getItem("reduxState"))
+  : {};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
   persistedState,
-   composeEnhancers(applyMiddleware(thunk)));
-store.subscribe(()=>{
-  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
-})
+  composeEnhancers(applyMiddleware(thunk))
+);
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 ReactDOM.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <App />
-  </Provider>
-  , document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
