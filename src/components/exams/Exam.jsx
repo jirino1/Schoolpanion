0c0c8b63f3255fc8bf3f26802cf1a7ff7f4dfef7
jirino1 <56354233+jirino1@users.noreply.github.com>
2020-03-02 19:28:06 +0100

    minor bug fixes

--- src/components/exams/Exam.jsx
+++ src/components/exams/Exam.jsx
@@ -20,88 +20,95 @@ class Exam extends Component {
     // Hier wird die Action ausgelöst,
     // die das To-Do lädt
     const { id } = this.props.match.params;
+    console.log(id);
     await this.props.getExam(id);
   }
 
   render() {
     const { exam } = this.props.exams;
     const tasks = this.props.tasks.list;
-    if (exam === null || tasks === null) {
+    console.log(exam);
+    if (
+      exam === null ||
+      tasks === null ||
+      exam.id === this.props.match.params.id
+    ) {
       return <div>Loading...</div>;
-    }
-    if (exam === undefined) {
+    } else if (exam === undefined) {
       return <div>The Exam you're searching for doesn't exist</div>;
-    }
-    return (
-      <div className="ui container">
-        <div className="ui card" key={exam.id}>
-          <div className="content">
-            <div className="header">{exam.subject}</div>
-            <div className="meta">{"Till " + formatDate(exam.date)}</div>
-            <div className="description">
-              <div className="ui relaxed divided list">
-                {exam.tasks.map(index => {
-                  console.log(index);
-                  const handledTask = tasks.find(t => t.id === index);
-                  return (
-                    <div key={index} className="item">
-                      <div className="content">
-                        <button
-                          onClick={() => {
-                            this.props.markDone(index);
-                          }}
-                          style={{
-                            float: "left",
-                            marginRight: "7px",
-                            backgroundColor: "white"
-                          }}
-                          className="ui icon button"
-                        >
-                          <i
-                            className={
-                              handledTask.completed
-                                ? "large check square icon"
-                                : "large check square outline icon"
-                            }
-                          ></i>
-                        </button>
-                        <div className="header">{handledTask.title}</div>
-                        <div className="description">
-                          {"Till " + formatDate(handledTask.date)}
+    } else {
+      const { exam } = this.props.exams;
+      const tasks = this.props.tasks.list;
+      return (
+        <div className="ui container">
+          <div className="ui card" key={exam.id}>
+            <div className="content">
+              <div className="header">{exam.subject}</div>
+              <div className="meta">{"Till " + formatDate(exam.date)}</div>
+              <div className="description">
+                <div className="ui relaxed divided list">
+                  {exam.tasks.map(task => {
+                    const handledTask = tasks.find(t => t.id === task);
+                    return (
+                      <div key={task} className="item">
+                        <div className="content">
+                          <button
+                            onClick={() => {
+                              this.props.markDone(task);
+                            }}
+                            style={{
+                              float: "left",
+                              marginRight: "7px",
+                              backgroundColor: "white"
+                            }}
+                            className="ui icon button"
+                          >
+                            <i
+                              className={
+                                handledTask.completed
+                                  ? "large check square icon"
+                                  : "large check square outline icon"
+                              }
+                            ></i>
+                          </button>
+                          <div className="header">{handledTask.title}</div>
+                          <div className="description">
+                            {"Till " + formatDate(handledTask.date)}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="ui bottom attached collapsing small icon buttons">
-            <button
-              className="ui small icon button"
-              onClick={() => {
-                history.push(`/exams/exam/${exam.id}/edit`);
-              }}
-            >
-              <i className="edit icon"></i>
-            </button>
-            <button
-              className="ui small icon button"
-              id={exam.id}
-              onClick={() => {
-                for (let i = 0; i < exam.tasks.length; i++) {
-                  this.props.deleteTask(exam.tasks[i]);
-                }
-                this.props.deleteExam(exam.id);
-                history.push("/exams");
-              }}
-            >
-              <i className="trash alternate outline icon"></i>
-            </button>
+            <div className="ui bottom attached collapsing small icon buttons">
+              <button
+                className="ui small icon button"
+                onClick={() => {
+                  history.push(`/exams/exam/${exam.id}/edit`);
+                }}
+              >
+                <i className="edit icon"></i>
+              </button>
+              <button
+                className="ui small icon button"
+                id={exam.id}
+                onClick={() => {
+                  for (let i = 0; i < exam.tasks.length; i++) {
+                    this.props.deleteTask(exam.tasks[i]);
+                  }
+                  this.props.deleteExam(exam.id);
+                  history.push("/exams");
+                }}
+              >
+                <i className="trash alternate outline icon"></i>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
