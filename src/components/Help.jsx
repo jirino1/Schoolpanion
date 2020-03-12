a3209f91c70e606707b18dcacf416d5b49264163
jirino1 <56354233+jirino1@users.noreply.github.com>
2020-03-12 09:37:50 +0100

    fix

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -19,7 +19,8 @@ const Help = () => {
         tollen Features um dir die Organisation von deinem Schulleben zu
         erleichtern. Du kannst Lernpläne für die Klausuren verwalten sowie
         Hausaufgaben und deinen Stundenplan eintragen. Auf der Hauptseite werden
-        dir bei allem Chaos kurz und kompakt die <b>5 nächsten anstehenden</b>
+        dir bei allem Chaos kurz und kompakt die{" "}
+        <b>{"5 nächsten anstehenden "}</b>
         Aufgaben von den Lernplänen und den Hausaufgaben angezeigt.
         <br />
         Anmerkung vom Entwickler: So stolz ich auch auf meine App bin: Ich bin
